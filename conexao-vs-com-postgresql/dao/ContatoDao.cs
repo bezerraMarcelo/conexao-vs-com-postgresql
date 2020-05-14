ae1ac7ace7211f7386c026d5f4c9909e8c02489a
marcelo.abezerra@gmail.com <bezerraMarcelo>
2020-05-14 12:27:56 -0300

    Ajustes no dao

--- conexao-vs-com-postgresql/dao/ContatoDao.cs
+++ conexao-vs-com-postgresql/dao/ContatoDao.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Modelo;
+using System;
 
 namespace dao
 {
     public class ContatoDao
     {
+        public void adiciona(Contato contato)
+        {
+
+        }
     }
 }
