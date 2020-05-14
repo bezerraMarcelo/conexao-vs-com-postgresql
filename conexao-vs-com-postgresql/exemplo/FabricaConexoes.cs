7bbe3be76b1271498ce5504917cceb718fe32d2a
marcelo.abezerra@gmail.com <bezerraMarcelo>
2020-05-14 14:16:44 -0300

    Criando fabrica de conexoes

---
+++ conexao-vs-com-postgresql/exemplo/FabricaConexoes.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System;
+
+namespace exemplo
+{
+    public class FabricaConexoes
+    {
+        public NpgsqlConnection conexao()
+        {
+            try
+            {
+                string stringConexao = "server=localhost;port=5432;user id=postgres;password=root;database=cliente";
+                return new NpgsqlConnection(stringConexao);
+
+            } catch (Exception e)
+            {
+                throw new System.ArgumentException("Erro ao conectar ao banco de dados");                
+            }
+            
+        }
+    }
+}
