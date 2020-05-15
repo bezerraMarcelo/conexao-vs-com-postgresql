9867cc0ee95cc5b357d1c98e61bcbacb9ec38fa4
marcelo.abezerra@gmail.com <bezerraMarcelo>
2020-05-14 23:00:24 -0300

    Adicionado o ler

--- conexao-vs-com-postgresql/dao/ContatoDao.cs
+++ conexao-vs-com-postgresql/dao/ContatoDao.cs
@@ -1,6 +1,8 @@
 ﻿using exemplo.modelo;
 using System;
 using Npgsql;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace exemplo.dao
 {
@@ -18,18 +20,49 @@ namespace exemplo.dao
             try
             {
                 string comando = "insert into contatos (nome, email) values (@p1, @p2)";
-                                
+
                 using (var cmd = new NpgsqlCommand(comando, conexao))
                 {
                     cmd.Parameters.AddWithValue("p1", contato.nome);
                     cmd.Parameters.AddWithValue("p2", contato.email);
                     cmd.ExecuteNonQuery();
                 }
-            } 
+            }
             finally
             {
                 conexao.Close();
-            }            
+            }
         }
-    }
+
+        public List<Contato> Ler()
+        {
+            List<Contato> lista =  new List<Contato>();
+            string comando = "select * from contatos";
+
+            using (var cmd = new NpgsqlCommand(comando, conexao))
+            {
+                conexao.Open();
+                try
+                {
+                    var leitura = cmd.ExecuteReader();
+                    while (leitura.Read())
+                    {
+                        Contato contato = new Contato();
+
+                        contato.id = Int32.Parse(leitura["id"].ToString());
+                        contato.nome = leitura["nome"].ToString();
+                        contato.email = leitura["email"].ToString();
+
+                        lista.Add(contato);
+                    }
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+            }
+            return lista;
+        }
+        
+     }
 }
---
+++ conexao-vs-com-postgresql/exemplo.teste-ler/Program.cs
@@ -0,0 +1,27 @@
+﻿using exemplo.dao;
+using exemplo.modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace exemplo.teste_ler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contato> contatos;
+                        
+            ContatoDao dao = new ContatoDao();
+
+            contatos = dao.Ler();
+
+            foreach(Contato contato in contatos)
+            {
+                Console.Out.WriteLine(contato.nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
